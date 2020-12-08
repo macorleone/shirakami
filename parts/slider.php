1209859780d0600e53cecad67350fe33d394f457
makoto-consis <saito@consis.jp>
2020-12-08 19:02:58 +0900

    slider sp_navi add

---
+++ parts/slider.php
@@ -0,0 +1,26 @@
+<div class="swiper-container">
+	<div class="swiper-wrapper">					
+					<picture class="swiper-slide">
+						<source srcset="<?php echo get_template_directory_uri(); ?>/img/eye_sp02.jpg" media="(max-width: 700px)">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/eye03.jpg" alt="0歳から100歳までともに生きる 七峰会・障がい者支援">
+					</picture>        
+					<picture class="swiper-slide">
+						<source srcset="<?php echo get_template_directory_uri(); ?>/img/eye_sp03.jpg" media="(max-width: 700px)">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/eye04.jpg" alt="穏やかに安心して暮らすためのサポート 障がい者支援・高齢者支援 しちほうかい">
+					</picture>
+					<picture class="swiper-slide">
+						<source srcset="<?php echo get_template_directory_uri(); ?>/img/eye_sp01.jpg" media="(max-width: 700px)">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/eye.jpg" alt="ができるだけ自立し、日常生活を送れるようサポート 七峰会・高齢者支援">
+					</picture>
+				</div>
+			</div>
+
+			<div class="wave">
+				<svg class="pc" viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+					<path d="M-11.57,110.03 C231.65,201.80 395.87,92.27 500.27,137.66 L500.00,150.00 L0.00,150.00 Z"
+						  style="stroke: none; fill: #ecf5fc;"></path>
+				</svg>
+				<svg class="sp" viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+					<path d="M-5.36,126.80 C128.94,110.03 297.11,135.69 509.87,134.70 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #ecf5fc;"></path>
+				</svg>
+			</div>
