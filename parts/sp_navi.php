9a2e1240c59281db15f607e0a1cfe45ae8f9da02
makoto-consis <saito@consis.jp>
2020-12-09 18:16:28 +0900

    sp整理

--- parts/sp_navi.php
+++ parts/sp_navi.php
@@ -5,9 +5,7 @@
 	<nav class="remodal" data-remodal-id="modal_menu">
 		<div>
 			<button data-remodal-action="close" class="remodal-close" aria-label="Close"><i class="fas fa-times"></i></button>
-			<a href="<?php esc_url(site_url('/')); ?>" class="logo">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/logo-footer.png" alt="logo">
-			</a>
+			<a href="<?php esc_url(site_url('/')); ?>" class="logo"><img src="<?php echo get_template_directory_uri(); ?>/img/logo-footer.png" alt="logo"></a>
 			<ul>
 				<li><a href="<?php echo esc_url(site_url('/strength')); ?>"><?php echo get_the_title(get_page_by_path('strength')); ?></a></li>
 				<li><a href="<?php echo esc_url(site_url('/corporate')); ?>"><?php echo get_the_title(get_page_by_path('corporate')); ?></a></li>
